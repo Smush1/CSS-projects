94450d00724a484a1a60a7d47ad69d8ad6583a78
unknown <Sumesh1712@gmail.com>
2023-02-12 15:12:37 +0530

    Some changes

---
+++ Javascript/Rock-paper-scissor/rock-paper.js
@@ -0,0 +1,11 @@
+const computerChoice = document.querySelector("#computer-choice")
+const userChoiceDisplay = document.querySelector("#user-choice")
+const result = document.querySelector("#result")
+const possiblechoices = document.querySelectorAll("button")
+
+possiblechoices.forEach(possiblechoices =>) {
+
+}
+
+let userChoice
+
